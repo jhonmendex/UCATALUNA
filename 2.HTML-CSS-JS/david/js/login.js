e957f5d33600ff0ef5fde06104303e67c57a019e
David Escorce <davidescorce03@gmail.com>
2024-05-30 19:49:32 -0500

    change: button to type submit and login validation

--- 2.HTML-CSS-JS/david/js/login.js
+++ 2.HTML-CSS-JS/david/js/login.js
@@ -2,14 +2,13 @@ const form = document.getElementById('form');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 let userData =  {
-	email: '',
-	password: ''
+	email: 'davidescorce03@gmail.com',
+	password: '1234'
 }
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	userData.email = emailInput.value;
-	userData.password = passwordInput.value;
-
-	alert(`email: ${userData.email} - password: ${userData.password}`)
+	if(userData.email === emailInput.value && userData.password === passwordInput.value) {
+		alert(`BIENVENIDO!!! ${userData.email}`)
+	}
 })
