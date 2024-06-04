96c9d06942d4a9ef3034a83e154b72a34acb25f0
jhon mendez <programacioncolaborativa2023@gmail.com>
2024-06-04 18:02:20 -0500

    add:new files clases and objects

--- 2.HTML-CSS-JS/david/js/login.js
+++ 2.HTML-CSS-JS/david/js/login.js
@@ -1,15 +1,14 @@
-const form = document.getElementById('form');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-let userData =  {
-	email: '',
-	password: ''
-}
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+let userData = {
+  email: "jhon@gmail.com",
+  password: "123",
+};
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	userData.email = emailInput.value;
-	userData.password = passwordInput.value;
-
-	alert(`email: ${userData.email} - password: ${userData.password}`)
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  userData.email = emailInput.value;
+  userData.password = passwordInput.value;
+  alert(`email: ${userData.email} - password: ${userData.password}`);
+});
