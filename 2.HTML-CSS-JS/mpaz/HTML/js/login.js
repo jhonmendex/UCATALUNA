5b53e5e2f75a6880744b8a61a7fbd1ea81ba8eb9
mary <m.paz1971@hotmail.com>
2024-06-05 21:10:12 -0500

    fix: import file usuarios.js

--- 2.HTML-CSS-JS/mpaz/HTML/js/login.js
+++ 2.HTML-CSS-JS/mpaz/HTML/js/login.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import Usuario from "./usuario.js";
 
-const usuarioBack = new Usuario("m.pazn@gmail.com", "123");
+const usuarioBack = new Usuario("jhon@gmail.com", "123");
 
 //2.capturar los datos de usuario y contrase;a DOM
 const usuarioFront = new Usuario();
@@ -37,23 +36,3 @@ function mostrarAlerta(mensaje, tipo) {
     alerta.style.display = "none";
   }, 5000);
 }
-=======
-const form = document.getElementById('form');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-let userData =  {
-	email: 'mpaz@gmail.com',
-	password: '1234'
-}
-
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	if(userData.email === emailInput.value && userData.password === passwordInput.value) {
-    	alert(`Pase al siguiente nivel ${userData.email}`)
-	}
-    else{
-        alert(`Lo sentimos ${userData.email} la clave es incorrecta`)
-    }
-    
-})
->>>>>>> 64772a2d9ea551f9dfa7fb2b052c1dbe13f42345
